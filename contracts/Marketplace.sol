3c5733522e7f59483a65a59e501406a3dc8956ad
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2021-10-20 16:51:05 -0400

    feat(marketplace): add contracts & tests

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,12 +1,119 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+contract Marketplace is IERC721Receiver, ReentrancyGuard {
+    struct Item {
+        address seller;
+        address owner;
+        uint256 price;
+    }
 
-contract Marketplace {
-    
+    struct Sell {
+        address buyer;
+        uint256 price;
+    }
 
-    constructor() {}
+    mapping(address => mapping(uint256 => Item)) private assets;
+    mapping(address => mapping(uint256 => Sell[])) private history;
 
-    
+    event AddedToMarket(
+        address indexed seller,
+        address indexed owner,
+        address indexed tokenAddress,
+        uint256 tokenId,
+        uint256 price
+    );
+    event RetiredFromMarket(
+        address indexed seller,
+        address indexed owner,
+        address indexed tokenAddress,
+        uint256 tokenId,
+        uint256 price
+    );
+
+    function sellNFT(
+        address _tokenAddress,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant returns (bool) {
+        require(_price >= 0.01 ether, "NFT price too low.");
+
+        Item memory nft = Item({
+            seller: msg.sender,
+            owner: address(0),
+            price: _price
+        });
+
+        assets[_tokenAddress][_tokenId] = nft;
+
+        IERC721(_tokenAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _tokenId
+        );
+
+        emit AddedToMarket(
+            msg.sender,
+            address(0),
+            _tokenAddress,
+            _tokenId,
+            _price
+        );
+
+        return true;
+    }
+
+    function buyNFT(address _tokenAddress, uint256 _tokenId)
+        external
+        payable
+        nonReentrant
+        returns (bool)
+    {
+        Item memory nft = assets[_tokenAddress][_tokenId];
+
+        require(nft.price == msg.value, "Price not met.");
+
+        (bool success, ) = nft.seller.call{value: msg.value}("");
+
+        require(success, "Transaction failed.");
+
+        assets[_tokenAddress][_tokenId] = Item({
+            seller: address(0),
+            owner: msg.sender,
+            price: 0
+        });
+
+        history[_tokenAddress][_tokenId].push(
+            Sell({buyer: msg.sender, price: nft.price})
+        );
+
+        IERC721(_tokenAddress).safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenId
+        );
+
+        emit RetiredFromMarket(
+            nft.seller,
+            msg.sender,
+            _tokenAddress,
+            _tokenId,
+            nft.price
+        );
+
+        return true;
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
 }
--- test/test.js
+++ test/test.js
@@ -2,11 +2,68 @@ const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
 describe('Marketplace', function () {
-  it('', async function () {
+  it('Should handle single item sell', async function () {
+    const signers = await ethers.getSigners()
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy('Marketplace NFT', 'MK')
+    await nft.deployed()
+
+    const tokenAddress = nft.address
+
+    await nft.createNFT('demo.marketplace.0')
+
+    expect(await nft.ownerOf(0)).to.equal(signers[0].address)
+
     const Marketplace = await ethers.getContractFactory('Marketplace')
-    const market = await Marketplace.deploy('Hello, world!')
+    const market = await Marketplace.deploy()
     await market.deployed()
 
-    // expect().to.equal();
+    await nft.approve(market.address, 0)
+
+    await market.sellNFT(tokenAddress, 0, ethers.utils.parseEther('0.01'))
+
+    expect(await nft.ownerOf(0)).to.equal(market.address)
+
+    await nft.createNFT('demo.marketplace.1')
+
+    expect(await nft.ownerOf(1)).to.equal(signers[0].address)
+
+    await expect(
+      market.sellNFT(tokenAddress, 1, ethers.utils.parseEther('0.01'))
+    ).to.be.revertedWith('ERC721: transfer caller is not owner nor approved')
+
+    expect(await nft.ownerOf(1)).to.equal(signers[0].address)
   })
+
+  it('Should handle single item buy', async function () {
+    const signers = await ethers.getSigners()
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy('Marketplace NFT', 'MK')
+    await nft.deployed()
+
+    const tokenAddress = nft.address
+
+    await nft.createNFT('demo.marketplace.0')
+
+    const Marketplace = await ethers.getContractFactory('Marketplace')
+    const market = await Marketplace.deploy()
+    await market.deployed()
+
+    await nft.approve(market.address, 0)
+
+    await market.sellNFT(tokenAddress, 0, ethers.utils.parseEther('0.01'))
+
+    await market
+      .connect(signers[1])
+      .buyNFT(tokenAddress, 0, { value: ethers.utils.parseEther('0.01') })
+
+    expect(await nft.ownerOf(0)).to.equal(signers[1].address)
+  })
+
+  // it('Show...', async function () {
+  //   const Marketplace = await ethers.getContractFactory('Marketplace')
+  //   const market = await Marketplace.deploy()
+  //   await market.deployed()
+
+  // })
 })
