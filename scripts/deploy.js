eb7971088c884d8233aed47800aa992ebb16bf7e
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2021-10-23 11:03:06 -0400

    chore(deploy): update contract info

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory('Greeter')
-  const greeter = await Greeter.deploy('Hello, Hardhat!')
+  const Marketplace = await hre.ethers.getContractFactory('Marketplace')
+  const market = await Marketplace.deploy()
 
-  await greeter.deployed()
+  await market.deployed()
 
-  console.log('Greeter deployed to:', greeter.address)
+  console.log('Marketplace deployed to:', market.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
